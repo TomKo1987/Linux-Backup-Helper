f461edbb0b9fb3562f8bb8a73c93828d07b8c33d
TomKo1987 <155864518+TomKo1987@users.noreply.github.com>
2025-06-10 17:16:49 +0200

    file_process.py

--- file_process.py
+++ file_process.py
@@ -12,7 +12,7 @@ from PyQt6.QtWidgets import (QDialog, QWidget, QVBoxLayout, QHBoxLayout, QLabel,
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-if not logger.handlers:
+if not logger.hasHandlers():
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
