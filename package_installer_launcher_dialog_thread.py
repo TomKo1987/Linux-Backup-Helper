ff7c4c7478553345aa363a3de801be02a415a2d7
TomKo1987 <155864518+TomKo1987@users.noreply.github.com>
2025-06-10 17:18:01 +0200

    package_installer_launcher_dialog_thread.py

--- package_installer_launcher_dialog_thread.py
+++ package_installer_launcher_dialog_thread.py
@@ -15,7 +15,7 @@ home_config = Path(home_user).joinpath(".config")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-if not logger.handlers:
+if not logger.hasHandlers():
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
