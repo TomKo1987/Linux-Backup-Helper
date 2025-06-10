500f095a6d2b4dd9414f7cf48f05e438812a5d87
TomKo1987 <155864518+TomKo1987@users.noreply.github.com>
2025-06-10 17:18:13 +0200

    package_installer_options.py

--- package_installer_options.py
+++ package_installer_options.py
@@ -11,7 +11,7 @@ from PyQt6.QtWidgets import (QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QFo
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-if not logger.handlers:
+if not logger.hasHandlers():
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
