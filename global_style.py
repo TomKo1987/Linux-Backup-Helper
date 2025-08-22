a6dc403539154ecf7df727ef803f2a763d7d204e
TomKo1987 <155864518+TomKo1987@users.noreply.github.com>
2025-08-22 15:02:48 +0200

    global_style.py

--- global_style.py
+++ global_style.py
@@ -94,7 +94,6 @@ QTextEdit, QListWidget {
     border-radius: 8px;
     font-size: 16px;
     font-family: "FiraCode Nerd Font Mono", "DejaVu Sans Mono", "Courier New", monospace;
-    outline: none;
 }
 QListWidget::item {
     padding: 5px;
