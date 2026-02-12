ab77a686ca8327d70b652e09a8159a2145413274
TomKo1987 <155864518+TomKo1987@users.noreply.github.com>
2026-02-12 18:18:11 +0100

    Create logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        logger.addHandler(handler)
+    return logger
